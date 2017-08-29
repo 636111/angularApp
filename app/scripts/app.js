3755a54b091a5ead88bbde18b3851a77b13b5bf5
Carmen Dominguez <MariaDelCarmen.DominguezCampillo@cognizant.com>
2017-08-29 17:00:37 +0200

    change route for each tab

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,17 @@
  * Main module of the application.
  */
 angular
-  .module('angularApp', []);
+  .module('angularApp', ['ngRoute'])
+  .config(['$routeProvider', function($routeProvider){
+    $routeProvider
+      .when('/', {
+        templateUrl : '../views/main.html'
+      })
+      .when('/about', {
+        templateUrl: '../views/about.html'
+      })
+      .when('/contact', {
+        templateUrl: '../views/contact.html'
+      })
+      .otherwise('/');
+  }]);
