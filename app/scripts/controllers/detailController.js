90f98e35f2cfcad2aafd8b101e368ae8a6052e10
Carmen Dominguez <MariaDelCarmen.DominguezCampillo@cognizant.com>
2017-09-04 17:53:45 +0200

    add getCountryById(id)

--- app/scripts/controllers/detailController.js
+++ app/scripts/controllers/detailController.js
@@ -2,18 +2,21 @@
 
 angular
   .module('angularApp')
-  .controller('detailController', ['$scope', 'countriesService', function ($scope, countriesService) {
+  .controller('detailController', ['$scope', 'countriesService','$routeParams', function ($scope, countriesService, $routeParams) {
 
     $scope.getCountryById = function(id) {
 
       countriesService.getCountries()
         .then(function(response){
           $scope.detailCountry = response.data.Response[id];
+          console.log($scope.detailCountry);
         }, function(error){
           $scope.status = 'Unable to load country info: ' + error.message;
         });
       
         //$scope.countries = countriesService.getCountries();
-    }
+    };
+
+    $scope.getCountryById($routeParams.id);
 
   }]);
